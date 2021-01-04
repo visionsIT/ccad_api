fd11475c5931ef51bfccc6afadcc9f4ba944e482
visionsIT <sales@visions.net.in>
2021-01-04 17:21:44 +0530

    mrning change1

--- Modules/Nomination/Http/Controllers/UserNominationController.php
+++ Modules/Nomination/Http/Controllers/UserNominationController.php
@@ -1944,7 +1944,7 @@ public function updateLevelOne(Request $request, $id): JsonResponse
         try {
             $rules = [
                 'account_id' => 'required|integer|exists:accounts,id',
-                //'campaign_id' => 'required|integer|exists:campaign_types,id',
+                 'campaign_id' => 'required|integer|exists:value_sets,id',
                 //'role_type' => 'required|integer|in:2,3',
             ];
 
