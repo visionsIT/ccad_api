98d1408a884e3fe9ada212711b936b13340d99bb
Narinder Singh <narinder@visions.net.in>
2021-01-08 19:59:55 +0530

    sso enable

--- app/Http/Middleware/CheckKafuBackendAuthToken.php
+++ app/Http/Middleware/CheckKafuBackendAuthToken.php
@@ -38,7 +38,7 @@ class CheckKafuBackendAuthToken
 
          if(isset($_REQUEST['SAMLResponse'])){
 
-             echo "<pre>"; print_r($_REQUEST); print_r($_COOKIE); die;
+             //echo "<pre>"; print_r($_REQUEST); print_r($_COOKIE); die;
             $useremail = ($_REQUEST['email'])?$_REQUEST['email']:'';//'lootahs@clevelandclinicabudhabi.ae';
             $account = Account::where('email', $useremail)->first();
 
