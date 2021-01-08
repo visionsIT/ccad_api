7aab50206cf8110acf37163be1177ac3d1b1db8f
Narinder Singh <narinder@visions.net.in>
2021-01-08 15:00:44 +0530

    sso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,9 @@
                 //     $("#div1").html(result);
                 // }});
             });
-            function readCookie(name) {
-                var nameEQ = name + "=";
-                var ca = document.cookie.split(';');
-                for(var i=0;i < ca.length;i++) {
-                    var c = ca[i];
-                    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                }
-                return null;
+            function readCookie() {
+                var ca = document.cookie
+                console.log(ca);
             }
         </script>
 
