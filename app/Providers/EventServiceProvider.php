c520d9aff49ba255fb709139d02b41794d79c9c4
Narinder Singh <narinder@visions.net.in>
2021-01-08 10:09:45 +0530

    sso fixing

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -38,13 +38,13 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-        die('Event');
+
         Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function ($event) {
             //$messageId = $event->getSaml2Auth()->getLastMessageId();
             // Add your own code preventing reuse of a $messageId to stop replay attacks
 
             $user = $event->getSaml2User();
-
+            die('Event');
             $userData = [
                 'id' => $user->getUserId(),
                 'attributes' => $user->getAttributes(),
