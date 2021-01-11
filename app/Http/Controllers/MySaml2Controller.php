683f53d6b086e737d161bf7eb583bef7dbd1b0f1
Narinder Singh <narinder@visions.net.in>
2021-01-11 14:53:51 +0530

    sso setting

--- app/Http/Controllers/MySaml2Controller.php
+++ app/Http/Controllers/MySaml2Controller.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Aacotroneo\Saml2\Http\Controllers\Saml2Controller;
+use Aacotroneo\Saml2\Events\Saml2LoginEvent;
+use Aacotroneo\Saml2\Saml2Auth;
+use Illuminate\Routing\Controller;
 
 class MySaml2Controller extends Saml2Controller
 {
@@ -13,4 +16,30 @@ class MySaml2Controller extends Saml2Controller
         $loginRedirect = '...'; // Determine redirect URL
         $this->saml2Auth->login($loginRedirect);
     }*/
+
+    public function acs(Saml2Auth $saml2Auth, $idpName)
+    {
+        $errors = $saml2Auth->acs();
+
+        if (!empty($errors)) {
+            logger()->error('Saml2 error_detail', ['error' => $saml2Auth->getLastErrorReason()]);
+            session()->flash('saml2_error_detail', [$saml2Auth->getLastErrorReason()]);
+
+            logger()->error('Saml2 error', $errors);
+            session()->flash('saml2_error', $errors);
+            return redirect(config('saml2_settings.errorRoute'));
+        }
+        $user = $saml2Auth->getSaml2User();
+
+        event(new Saml2LoginEvent($idpName, $user, $saml2Auth));
+
+        $redirectUrl = $user->getIntendedUrl();
+
+        if ($redirectUrl !== null) {
+            return redirect($redirectUrl);
+        } else {
+
+            return redirect(config('saml2_settings.loginRoute'));
+        }
+    }
 }
