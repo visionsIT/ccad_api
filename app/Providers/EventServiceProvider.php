7dc43fee7001050cb9e97968deee1047948a6cb3
Narinder Singh <narinder@visions.net.in>
2021-01-21 15:55:42 +0530

    sso setting

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -67,24 +67,24 @@ class EventServiceProvider extends ServiceProvider
                     //$roleInfo =  DB::table('model_has_roles')->select('roles.*')->join('roles', 'roles.id', '=', 'model_has_roles.role_id')->where(['model_has_roles.model_id' => $account->id])->get()->first();
                     $userInfo = DB::table('program_users')->where('account_id', $account->id)->first();
                     if(empty($userInfo)){
-                        header("Location: https://ccadapi.takreem.ae/login/not-allowed");
+                        header("Location: https://ccad.meritincentives.com/login/not-allowed");
                         exit;
                     } else {
                         $successToken =  $account->createToken('userToken'.$account->id)->accessToken;
-                        header("Location: https://ccadapi.takreem.ae/login/".$successToken);
+                        header("Location: https://ccad.meritincentives.com/login/".$successToken);
                     }
                 } else {
-                    header("Location: https://ccadapi.takreem.ae/login/not-active");
+                    header("Location: https://ccad.meritincentives.com/login/not-active");
                     exit;
                 }
             } else {
-               header("Location: https://ccadapi.takreem.ae/login/not-exist");
+               header("Location: https://ccad.meritincentives.com/login/not-exist");
             }
             exit();
         });
 
         Event::listen('Aacotroneo\Saml2\Events\Saml2LogoutEvent', function ($event) {
-           header("Location: https://ccadapi.takreem.ae/login/");
+           header("Location: https://ccad.meritincentives.com/login/");
            exit();
             // Auth::logout();
             // Session::save();
